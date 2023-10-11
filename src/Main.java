7395f3018709fcbabc2e01b68feb27f660bb5cf2
Hugo1104 <43482934+Hugo1104@users.noreply.github.com>
2023-10-11 16:40:07 -0500

    10/11

--- src/Main.java
+++ src/Main.java
@@ -2470,6 +2470,165 @@ public class Main {
         return slow;
     }
 
+    public boolean searchMatrixReview(int[][] matrix, int target) {
+        // write your code here
+        if (matrix.length == 0) {
+            return false;
+        }
+
+        int m = matrix.length, n = matrix[0].length;
+        int low = 0, high = m * n - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int num = matrix[mid / n][mid % n];
+
+            if (num < target) {
+                low = mid + 1;
+            } else if (num > target) {
+                high = mid - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int searchMatrixII(int[][] matrix, int target) {
+        // write your code here
+        if (matrix.length == 0) {
+            return 0;
+        }
+
+        int i = matrix.length - 1, j = 0;
+        int counter = 0;
+
+        while (i >= 0 && j < matrix[0].length) {
+            int num = matrix[i][j];
+
+            if (num < target) {
+                j++;
+            } else if (num > target) {
+                i--;
+            } else {
+                i--;
+                j++;
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public int minArea(char[][] image, int x, int y) {
+
+        if (image == null || image.length == 0 || image[0].length == 0) {
+            return 0;
+        }
+
+
+
+        int n = image.length;
+        int m = image[0].length;
+        int l = 0, r = 0;
+        int left = 0, right = 0, top = 0, bottom = 0;
+
+        //二分最左侧黑色像素坐标
+        l = 0;
+        r = y;
+        while (l + 1 < r) {
+            int mid = l + (r - l) / 2;
+
+            if (check_column(image, mid)) {
+                r = mid;
+            } else {
+                l = mid;
+            }
+        }
+
+        if (check_column(image, l)){
+            left = l;
+
+        }else{
+            left = r;
+        }
+
+        //二分最右侧黑色像素坐标
+        l = y;
+        r = m - 1;
+
+        while (l + 1 < r) {
+            int mid = l + (r - l) / 2;
+            if (check_column(image, mid)) {
+                l = mid;
+            } else {
+                r = mid;
+            }
+        }
+
+        if (check_column(image, r)) {
+            right = r;
+        }else{
+            right = l;
+        }
+
+        //二分最上侧黑色像素坐标
+        l = 0;
+        r = x;
+        while (l + 1 < r) {
+            int mid = l + (r - l) / 2;
+            if (check_row(image, mid, left, right)) {
+                r = mid;
+            } else {
+                l = mid;
+            }
+        }
+
+        if (check_row(image, l, left, right)) {
+            top = l;
+        }else{
+            top = r;
+        }
+        //二分最下侧黑色像素坐标
+        l = x;
+        r = n - 1;
+        while (l + 1 < r) {
+            int mid = l + (r - l) / 2;
+            if (check_row(image, mid, left, right)) {
+                l = mid;
+            } else {
+                r = mid;
+            }
+        }
+
+        if (check_row(image, r, left, right)) {
+            bottom = r;
+        }else{
+            bottom = l;
+        }
+        return (right - left + 1) * (bottom - top + 1);
+    }
+
+    //判断列上是否存在黑色像素
+
+    private boolean check_column(char[][] image, int col) {
+        for (int i = 0; i < image.length; i++) {
+            if (image[i][col] == '1') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //判断行上是否存在黑色像素
+    private boolean check_row(char[][] image, int row ,int left ,int right) {
+        for (int j = left; j <= right; j++) {
+            if (image[row][j] == '1') {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 }
 
