2fbc4cab6d7a38e5623ecc31b85c3361cc56508a
Hugo1104 <43482934+Hugo1104@users.noreply.github.com>
2023-10-12 21:56:11 -0500

    10/12

--- src/Main.java
+++ src/Main.java
@@ -2630,6 +2630,32 @@ public class Main {
     }
 
 
+    public int[] intersection(int[] nums1, int[] nums2) {
+        // write your code here
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int index = 0, index1 = 0, index2 = 0;
+        int[] intersection = new int[nums1.length + nums2.length];
+
+        while (index1 < nums1.length && index2 < nums2.length) {
+            if (nums1[index1] == nums2[index2]) {
+                if (index == 0 || nums1[index1] != intersection[index - 1]) {
+                    intersection[index++] = nums1[index1];
+                }
+                index1++;
+                index2++;
+            } else if (nums1[index1] < nums2[index2]) {
+                index1++;
+            } else {
+                index2++;
+            }
+        }
+
+        return Arrays.copyOfRange(intersection, 0, index);
+    }
+
+
 }
 
 class Interval {
