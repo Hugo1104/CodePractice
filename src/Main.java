483167220eedc2a551598ac985bb284860d01399
Hugo1104 <43482934+Hugo1104@users.noreply.github.com>
2023-10-13 23:23:53 -0500

    10/13

--- src/Main.java
+++ src/Main.java
@@ -2655,6 +2655,28 @@ public class Main {
         return Arrays.copyOfRange(intersection, 0, index);
     }
 
+    public int minDepth(TreeNode root) {
+        // write your code here
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        int minDepth = Integer.MAX_VALUE;
+        if (root.left != null) {
+            minDepth = Math.min(minDepth(root.left), minDepth);
+        }
+
+        if (root.right != null) {
+            minDepth = Math.min(minDepth(root.right), minDepth);
+        }
+
+        return minDepth + 1;
+    }
+
 
 }
 
