a50b8dc02f32a005c0da430e9f0d2012358f48e6
hugoyin <zy1265@nyu.edu>
2023-10-20 20:47:37 -0700

    10/20

--- src/Main.java
+++ src/Main.java
@@ -2940,6 +2940,38 @@ public class Main {
     }
 
 
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        // write your code here
+        List<List<Integer>> ans = new LinkedList<>();
+        if (root == null) {
+            return ans;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                TreeNode left = node.left;
+                TreeNode right = node.right;
+                if (left != null) {
+                    queue.offer(left);
+                }
+
+                if (right != null) {
+                    queue.offer(right);
+                }
+            }
+            ans.add(0, level);
+        }
+
+        return ans;
+    }
+
 
 }
 
