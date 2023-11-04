e9a692ae5d824317d5e908f446c3dfe3fa04b777
hugoyin <zy1265@nyu.edu>
2023-11-03 23:00:31 -0700

    11/03

--- src/Main.java
+++ src/Main.java
@@ -3557,6 +3557,47 @@ public class Main {
     }
 
 
+    public int totalNQueens(int n) {
+        // write your code here
+        Set<Integer> columns = new HashSet<>();
+        Set<Integer> diagonals1 = new HashSet<>();
+        Set<Integer> diagonals2 = new HashSet<>();
+        return totalNQueensBacktrack(n, 0, columns, diagonals1, diagonals2);
+    }
+
+    private int totalNQueensBacktrack(int n, int row, Set<Integer> columns, Set<Integer> diagonals1, Set<Integer> diagonals2) {
+        if (row == n) {
+            return 1;
+        } else {
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                if (columns.contains(i)) {
+                    continue;
+                }
+                int diagonal1 = row - i;
+                if (diagonals1.contains(diagonal1)) {
+                    continue;
+                }
+                int diagonal2 = row + i;
+                if (diagonals2.contains(diagonal2)) {
+                    continue;
+                }
+                columns.add(i);
+                diagonals1.add(diagonal1);
+                diagonals2.add(diagonal2);
+
+                count += totalNQueensBacktrack(n, row + 1, columns, diagonals1, diagonals2);
+
+                columns.remove(i);
+                diagonals1.remove(diagonal1);
+                diagonals2.remove(diagonal2);
+            }
+
+            return count;
+        }
+    }
+
+
 
 }
 
