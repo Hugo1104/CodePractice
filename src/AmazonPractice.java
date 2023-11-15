9f8e3545df8b137d973350d9001511ba4dcd6cf1
Hugo1104 <43482934+Hugo1104@users.noreply.github.com>
2023-11-15 01:55:13 -0600

    11/14

--- src/AmazonPractice.java
+++ src/AmazonPractice.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AmazonPractice {
 
     public ListNode reverse(ListNode head) {
@@ -25,6 +28,48 @@ public class AmazonPractice {
         }
 
         return builder.toString();
+    }
+
+
+    public int minTotalDistance(int[][] grid) {
+        List<Integer> rows = collectRows(grid);
+        List<Integer> cols = collectCols(grid);
+        return minDistance1D(rows) + minDistance1D(cols);
+    }
+
+    private int minDistance1D(List<Integer> points) {
+        int distance = 0;
+        int i = 0;
+        int j = points.size() - 1;
+        while (i < j) {
+            distance += points.get(j) - points.get(i);
+            i++;
+            j--;
+        }
+        return distance;
+    }
+    private List<Integer> collectRows(int[][] grid) {
+        List<Integer> rows = new ArrayList<>();
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                if (grid[row][col] == 1) {
+                    rows.add(row);
+                }
+            }
+        }
+        return rows;
+    }
+
+    private List<Integer> collectCols(int[][] grid) {
+        List<Integer> cols = new ArrayList<>();
+        for (int col = 0; col < grid[0].length; col++) {
+            for (int row = 0; row < grid.length; row++) {
+                if (grid[row][col] == 1) {
+                    cols.add(col);
+                }
+            }
+        }
+        return cols;
     }
 
 }
