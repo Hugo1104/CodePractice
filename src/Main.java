aa2f6d3fa0a49a477150ccb77b4b52de3d8c0bef
hugoyin <zy1265@nyu.edu>
2023-12-09 21:02:36 +0800

    12/09

--- src/Main.java
+++ src/Main.java
@@ -5130,6 +5130,87 @@ public class Main {
         return grid[x][y] == GridType.EMPTY || grid[x][y] == GridType.HOUSE;
     }
 
+    public String alienOrder(String[] words) {
+        // Write your code here
+        if(words == null || words.length == 0){
+            return "";
+        }
+
+        Map<Character, Set<Character>> neighbors =
+                new HashMap<>();
+        Map<Character, Integer> indegrees =
+                new HashMap<>();
+
+        // build graph
+        // build nodes
+        for(int i = 0; i < words.length; i++){
+            for(int j = 0; j < words[i].length(); j++){
+                if(!neighbors.containsKey(words[i].charAt(j))){
+                    neighbors.put(words[i].charAt(j), new HashSet<Character>());
+                }
+
+                indegrees.put(words[i].charAt(j), 0);
+            }
+        }
+
+        // build edges
+        for(int i = 0; i < words.length - 1; i++){
+            //这里j和i的关系一定是相邻的两个单词
+            int j = i + 1;
+            int k = 0;
+
+            while(k < words[i].length() && k < words[j].length()){
+                if(words[i].charAt(k) != words[j].charAt(k)){
+                    neighbors.get(words[i].charAt(k)).add(words[j].charAt(k));
+
+                    break;
+                }
+
+                k++;
+            }
+        }
+
+        // build indegrees
+        // 这一步其实也可以写在建neighbors的时候, 但是那样写代码太混乱了, 这样清楚一些
+        for(char key : neighbors.keySet()){
+            for(char neighbor : neighbors.get(key)){
+                indegrees.put(neighbor, indegrees.get(neighbor) + 1);
+            }
+        }
+
+        // toplogical sorting
+        PriorityQueue<Character> queue = new PriorityQueue<>();
+
+        for(char key : indegrees.keySet()){
+            if(indegrees.get(key) == 0){
+                queue.add(key);
+            }
+        }
+
+        StringBuilder res = new StringBuilder();
+        while(!queue.isEmpty()){
+            char cur = queue.remove();
+            res.append(cur);
+
+            for(char neighbor : neighbors.get(cur)){
+                int new_indegree = indegrees.get(neighbor) - 1;
+                if(new_indegree == 0){
+                    queue.add(neighbor);
+                }
+
+                indegrees.put(neighbor, new_indegree);
+            }
+        }
+
+        //最后还要判断排出来的拓扑序是不是有效的, 是不是所有的节点都遍历过了
+        //如果不是的话我们直接输出空串, 证明组成给定的这些单词的字母没有办法组成一个合理的拓扑序
+        if(neighbors.keySet().size() != res.length()){
+            return "";
+        }
+
+        return res.toString();
+    }
+
 }
 
 class GridType {
