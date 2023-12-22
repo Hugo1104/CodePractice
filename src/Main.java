d6171d62baa73f30c2db64eedf39ffb262bb1428
hugoyin <zy1265@nyu.edu>
2023-12-22 11:51:42 +0800

    12/22 (1)

--- src/Main.java
+++ src/Main.java
@@ -5610,6 +5610,65 @@ public class Main {
         return dp[m - 1][n - 1];
     }
 
+    public int minPathSum(int[][] grid) {
+        // write your code here
+        if (grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
+        int m = grid.length, n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+
+    public int longestIncreasingContinuousSubsequence(int[] a) {
+        // write your code here
+        if (a == null || a.length == 0) {
+            return 0;
+        }
+
+        int ans = 1;
+        int length = 1;
+
+        for (int i = 1; i < a.length; i++) {
+            if (a[i - 1] < a[i]) {
+                length++;
+            } else {
+                length = 1;
+            }
+
+            ans = Math.max(ans, length);
+        }
+
+        length = 1;
+        for (int i = a.length - 2; i >= 0 ; i--) {
+            if (a[i + 1] < a[i]) {
+                length++;
+            } else {
+                length = 1;
+            }
+
+            ans = Math.max(ans, length);
+        }
+
+        return ans;
+    }
+
 }
 
 class RandomListNode {
