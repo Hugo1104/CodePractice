5e0b7d3d8b163396704b13658b13937e5b226251
hugoyin <zy1265@nyu.edu>
2023-12-22 17:11:22 +0800

    12/22 (2)

--- src/Main.java
+++ src/Main.java
@@ -5669,6 +5669,52 @@ public class Main {
         return ans;
     }
 
+    public int maxEnvelopes(int[][] envelopes) {
+        // write your code here
+        if (envelopes.length == 0 || envelopes[0].length == 0) {
+            return 0;
+        }
+
+        int n = envelopes.length;
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] != o2[0]) {
+                    return o1[0] - o2[0];
+                } else {
+                    return o2[1] - o1[1];
+                }
+            }
+        });
+
+        List<Integer> f = new ArrayList<>();
+        f.add(envelopes[0][1]);
+        for (int i = 1; i < n; i++) {
+            int num = envelopes[i][1];
+            if (num > f.get(f.size() - 1)){
+                f.add(num);
+            } else {
+                int index = maxHelper(f, num);
+                f.set(index, num);
+            }
+        }
+        return f.size();
+    }
+
+    private int maxHelper(List<Integer> f, int target) {
+        int low = 0, high = f.size() - 1;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (f.get(mid) < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+
 }
 
 class RandomListNode {
